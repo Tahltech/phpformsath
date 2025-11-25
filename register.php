4fbf01454e2faecf941f6c8627382e5b4a8482c0
Tahla Ebenezer Burinyuy <tahltech@Tahla.taltech>
2025-11-25 06:11:31 +0100

    second commit

--- register.php
+++ register.php
@@ -8,11 +8,10 @@ require_once __DIR__ . "/functions.php";
 
 session_start();
 
-if($_SERVER['REQUEST_METHOD'] !== 'POST') {
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     $_SESSION['error'] = "Invalid Request Method";
     redirect("./index.php");
-}
-else {
+} else {
 
     // echo "<pre>";
     // print_r($_POST);
@@ -34,27 +33,25 @@ else {
 
 
 
-    if(empty($fullName) || empty($emailAddress) || empty($password) || empty($gender) || empty($phoneNumber) || empty($address) || empty($placeOfBirth)|| empty($dateOfBirth)){
-    
+    if (empty($fullName) || empty($emailAddress) || empty($password) || empty($gender) || empty($phoneNumber) || empty($address) || empty($placeOfBirth) || empty($dateOfBirth)) {
+
         echo "check the fields and try again";
-        
+
         return redirect("/index.php");
     }
-
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
     $query = $connection->prepare("INSERT INTO users(name,email,gender,password,phone_number,address,date_of_birth,place_of_birth)
     VALUES(?,?,?,?,?,?,?,?)");
-    
-    $query->bind_param("ssssssss",$fullName, $emailAddress,$gender,$password,$phoneNumber, $address, $dateOfBirth, $placeOfBirth);
+
+    $query->bind_param("ssssssss", $fullName, $emailAddress, $gender, $hashedPassword, $phoneNumber, $address, $dateOfBirth, $placeOfBirth);
 
     $execution = $query->execute();
 
-    if ($execution){
-        echo "user created successfully";
+    if ($execution) {
+        
+    $query = ['success' => "user_created succesfully"];
+    $data = http_build_query($query);
+    redirect("./login.php?data=$data");
     }
-
 }
-
-
-
-
